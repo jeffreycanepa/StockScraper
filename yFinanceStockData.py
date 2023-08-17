3d99ce6146fe1493be6b6555f39686a7fa05313b
Jeff Canepa <jeff.canepa@gmail.com>
2023-08-17 16:37:19 -0700

    Added methods checkStocks(writer) and main()

--- yFinanceStockData.py
+++ yFinanceStockData.py
@@ -14,35 +14,46 @@ myStocks = ['AAPL', 'ADBE', 'CSCO', 'NTAP', 'MSFT', 'AMZN', 'TSLA', 'VMW', 'DOCU
 # Get current date
 now = datetime.datetime.now().strftime('%Y-%m-%d')
 
-# Create/open csv file for storing data
-try:
-    file = open('myStocks.csv', 'x')
-    writer = csv.writer(file)
-    writer.writerow(['Date', 'Ticker', 'Current Price', 'Previous Close', 'Change', '% Change'])
-    writer.writerow([now, '', '', '','',''])
-except FileExistsError as e:
-    file = open('myStocks.csv', 'a')
-    writer = csv.writer(file)
-    writer.writerow([now, '', '', '','',''])
 
-for stocks in myStocks:
-    ticker = yf.Ticker(stocks).info
-    curr_symbol = ticker['symbol']
-    # For reasons unknown to me, XLU does not have currentPrice as part of Yahoo Finance ticker info.
-    # Use navPrice instead
-    if curr_symbol == 'XLU':
-        current_price = ticker['navPrice']
-    else:
-        current_price = ticker['currentPrice']
-    previous_close_price = ticker['regularMarketPreviousClose']
-    
-    # Calculate the change in price
-    change = current_price - previous_close_price
-    
-    # Calculate the percentage change
-    pctChange = ((current_price - previous_close_price) / previous_close_price) * 100
-    
-    # Write out the data
-    writer.writerow(['', curr_symbol, current_price, previous_close_price, format(change, '.2f') , format(pctChange, '.2f')])
+def checkStocks(writer):
+    global myStocks
 
-file.close()
+    for stocks in myStocks:
+        ticker = yf.Ticker(stocks).info
+        curr_symbol = ticker['symbol']
+        # For reasons unknown to me, XLU does not have currentPrice as part of Yahoo Finance ticker info.
+        # Use navPrice instead
+        if curr_symbol == 'XLU':
+            current_price = ticker['navPrice']
+        else:
+            current_price = ticker['currentPrice']
+        previous_close_price = ticker['regularMarketPreviousClose']
+    
+        # Calculate the change in price
+        change = current_price - previous_close_price
+    
+        # Calculate the percentage change
+        pctChange = ((current_price - previous_close_price) / previous_close_price) * 100
+    
+        # Write out the data
+        writer.writerow(['', curr_symbol, current_price, previous_close_price, format(change, '.2f') , format(pctChange, '.2f')])
+
+
+def main():
+    # Create/open csv file for storing data
+    try:
+        file = open('myStocks.csv', 'x')
+        writer = csv.writer(file)
+        writer.writerow(['Date', 'Ticker', 'Current Price', 'Previous Close', 'Change', '% Change'])
+        writer.writerow([now, '', '', '','',''])
+    except FileExistsError as e:
+        file = open('myStocks.csv', 'a')
+        writer = csv.writer(file)
+        writer.writerow([now, '', '', '','',''])
+    
+    checkStocks(writer)
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
