88b4ecae41f5e328e16b812cae1ea8e4940a971e
Jeff Canepa <jeff.canepa@gmail.com>
2023-08-31 18:08:57 -0700

    Added trend lines to plot

--- plotStocks.py
+++ plotStocks.py
@@ -12,6 +12,7 @@
 -       tkinter
 -       matplotlib
 -       seaborn
+-       pandas
 -       datetime
 -
 -   Methods:
@@ -44,6 +45,7 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import seaborn as sns; sns.set()
+import pandas as pd
 from datetime import datetime, timedelta
 
 #Global variables
@@ -117,8 +119,27 @@ def plot_data(company, linestyle, window):
     # Create plot using matplotlib
     fig = plt.figure(figsize=(13, 7))
     sns.set_style('darkgrid')
+    
+    # convert the regression line start date to ordinal
+    x1 = pd.to_datetime(dates[0]).toordinal()
+
     for cmp, lstyle in zip(company,linestyle):
-        sns.lineplot(data=cmp,x="Date",y="Close",color=lstyle[0],linestyle=lstyle[1], label=cmp.name)
+        # convert the datetime index to ordinal values, which can be used to plot a regression line
+        cmp.index = cmp.index.map(pd.Timestamp.toordinal)
+        data=cmp.loc[x1:].reset_index()
+
+        # Add Closing price for stock as a line and as a linear regression (trend line)
+        ax1 = sns.lineplot(data=cmp,x=cmp.index,y="Close",color=lstyle[0],linestyle=lstyle[1], label=cmp.name)
+        sns.regplot(data=data, x=cmp.index, y='Close', color=lstyle[0], scatter=False, ci=False)
+
+        ax1.set_xlim(cmp.index[0], cmp.index[-1])
+
+        # convert the axis back to datetime
+        xticks = ax1.get_xticks()
+        labels = [pd.Timestamp.fromordinal(int(label)).date() for label in xticks]
+        ax1.set_xticks(xticks)
+        ax1.set_xticklabels(labels)
+
     sns.despine()
     plt.title('Closing Prices\n{0} - {1}'.format(dates[2], dates[3]), size='x-large', color='black')
     plt.ylabel('Stock Price $ (USD)')
