d8365f90b69a16f4d527319b0675c77c9969be28
Jeff Canepa <jeff.canepa@gmail.com>
2023-09-06 21:31:18 -0700

    Code to get Column name/data in Pandas

--- set_date_as_col_in_yfinance.py
+++ set_date_as_col_in_yfinance.py
@@ -2,4 +2,10 @@ import yfinance as yf
 tickers = ["AAPL"] 
 df = yf.download(tickers,  start = "2021-02-01" , end = "2021-02-04") 
 newdf = df.reset_index() 
-print(newdf)
+## Get the column names
+# for col in newdf.columns:
+#     print(col)
+
+# Set list to values in Column 'Date'
+myDates = newdf.loc[:,'Date']
+print(myDates)
