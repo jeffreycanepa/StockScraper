73201f30261c9134d4ba79abb6e0680684177541
Jeff Canepa <jeff.canepa@gmail.com>
2023-09-12 10:38:11 -0700

    Move set_date_as_col_in_yfinance to folder script bits

--- script bits/set_date_as_col_in_yfinance.py
+++ script bits/set_date_as_col_in_yfinance.py
@@ -1,6 +1,6 @@
 import yfinance as yf 
 tickers = ["AAPL"] 
-df = yf.download(tickers,  start = "2021-02-01" , end = "2021-02-04") 
+df = yf.download(tickers,  start = "2023-09-01" , end = "2023-09-12") 
 newdf = df.reset_index() 
 ## Get the column names
 # for col in newdf.columns:
@@ -8,4 +8,7 @@ newdf = df.reset_index()
 
 # Set list to values in Column 'Date'
 myDates = newdf.loc[:,'Date']
+x = len(myDates) -1
 print(myDates)
+print(myDates[0])
+print(myDates[len(myDates)-1])
