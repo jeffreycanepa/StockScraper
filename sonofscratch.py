9ee3af68247166464d2b5ea2d8548b00ce8bb8d6
Jeff Canepa <jeff.canepa@gmail.com>
2023-09-12 10:44:31 -0700

    Zeroed out data in script.

--- sonofscratch.py
+++ sonofscratch.py
@@ -1,14 +1,1 @@
-import tkinter as tk
-from tkinter.simpledialog import askinteger
-
-def main():
-    root = tk.Tk()
-    root.withdraw()
-    prompt = askinteger("Input", "Input an Integer")
-    root.destroy()
-    root.mainloop()
-
-    print(prompt)
-
-if __name__ == "__main__":
-    main()
+# A second scratch script for testing ideas before integrating into larger scripts
