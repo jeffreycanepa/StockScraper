13eaf416b84e23efdf094e5451f1f79519932730
Jeff Canepa <jeff.canepa@gmail.com>
2023-09-13 10:59:42 -0700

    Added getwinsize() which will aid in sizing the tkinter dialog based on how many companies there are

--- script bits/tk_checkboxes.py
+++ script bits/tk_checkboxes.py
@@ -11,28 +11,36 @@ import tkinter as tk
 
 companies = [['Apple','AAPL'],['Adobe','ADBE'],['Cisco','CSCO'],['NetApp','NTAP'],
              ['Microsoft','MSFT'],['Amazon','AMZN'],['Tesla','TSLA'],['VMWare','VMW'],
-             ['DocuSign','DOCU'],['Splunk','SPLK'],['XLU Utilities','XLU']] 
-tickers = ['AAPL','ADBE','CSCO','NTAP','MSFT','AMZN','TSLA','VMW','DOCU','SPLK','XLU']
+             ['DocuSign','DOCU'],['Splunk','SPLK'],['XLU Utilities','XLU'],['Broadcom','AVGO']] 
+tickers = ['AAPL','ADBE','CSCO','NTAP','MSFT','AMZN','TSLA','VMW','DOCU','SPLK','XLU','AVGO']
 company_names = ['Apple','Adobe','Cisco','NetApp','Microsoft','Amazon','Tesla',
-                 'VMWare', 'DocuSign', 'Splunk', 'XLU Utilities']
+                 'VMWare', 'DocuSign', 'Splunk', 'XLU Utilities','Broadcom']
+
 cbuts = []
 selectedCompanies = []
 btvars = []
 tl = None
+winsize = None
+
+def getwinsize():
+    winHeight = 120 + (len(company_names) * 23)
+    winGeometry = '200x{}+200+40'
+    return winGeometry.format(winHeight)
 
 def getselecteCompanies(company_names):
+    winsize = getwinsize()
     window = tk.Tk()
-    window.title('My Window')
-    window.geometry('200x460')
+    window.title('Select')
+    window.geometry(winsize)
     tline = tk.IntVar()
     cb = tk.IntVar()
 
     # Create a LabelFrame
-    frame =tk.LabelFrame(window, text="Select the Companies", padx=20, pady=20)
-    frame.pack(padx=10) #pady=20, padx=10
+    frame =tk.LabelFrame(window, text="Select Companies", padx=5, pady=5)
+    frame.pack(padx=10, pady=10)
 
     # Create a frame for checkboxes
-    frame2 = tk.Frame(window)
+    frame2 = tk.Frame(window, padx=17)
     frame2.pack()
 
     # Add method to select all checkboxes
@@ -50,16 +58,15 @@ def getselecteCompanies(company_names):
             tl = 1
 
     # array of the button values
-    for x in range(11):
+    for x in range(len(company_names)):
         btvars.append(tk.IntVar())
 
     for index, item in enumerate(company_names):
         cbuts.append(tk.Checkbutton(frame, text=item, anchor='w', width=50, variable=btvars[index], onvalue=1, offvalue=0, command=tline))
         cbuts[index].pack()
-    tk.Checkbutton(frame2, text='Select All', anchor='w', width=15, variable=cb, onvalue=1, offvalue=0, command=select_all).pack()
+    tk.Checkbutton(frame2, text='Select All', anchor='w', width=50, variable=cb, onvalue=1, offvalue=0, command=select_all).pack()
     # tk.Checkbutton(frame2, text='Display Trendline', anchor='w', width=15, variable=tline, onvalue=1, offvalue=0, command=showline).pack()
     tk.Button(window, text='Enter', command=lambda:[setSelectedCompanies(), window.destroy()]).pack()
-    print(frame.winfo_children())            
     window.mainloop()
 
 def setSelectedCompanies():
