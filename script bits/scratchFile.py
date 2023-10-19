9997b24d419f81608ff827c28f0d357b98bef258
Jeff Canepa <jeff.canepa@gmail.com>
2023-10-19 00:08:42 -0700

    Updated Scratch file to practice customtkinter

--- script bits/scratchFile.py
+++ script bits/scratchFile.py
@@ -1,1 +1,22 @@
 # Scratch script for practicing code before adding to larger script
+import tkinter
+import customtkinter
+
+app = customtkinter.CTk()
+app.geometry('400x200')
+app.title('Get Stock Ticker')
+
+label = customtkinter.CTkLabel(app, text='Enter Stock Ticker')
+label.pack(padx=10, pady=10)
+
+myTicker = tkinter.StringVar()
+ticker = customtkinter.CTkEntry(app, width=200, height=10, textvariable=myTicker)
+ticker.pack()
+
+def getTicker():
+    print(myTicker)
+
+btn = customtkinter.CTkButton(app, text="Enter", command=getTicker)
+btn.pack(padx=10,pady=20)
+
+app.mainloop()
