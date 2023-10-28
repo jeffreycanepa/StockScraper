51ada44f4ba655f77dec3955ac570562eca6e179
Jeff Canepa <jeff.canepa@gmail.com>
2023-10-27 17:56:34 -0700

    Updated with app description and requirements

--- plotOneStock_v2.py
+++ plotOneStock_v2.py
@@ -1,4 +1,43 @@
-# Scratch script for practicing code before adding to larger script
+# /opt/homebrew/bin/python3
+'''
+--------------------------------------------------------------
+-   plotOneStock_v2.py
+-       This script looks up the closing price for a stock 
+-       that the user supplies by calling yfinance. The user
+-       enters a stock ticker, then supplies the number of days
+-       to look stock data for.  The script then uses matplotlib 
+-       to plot the data within a tkinter window 
+-
+-   Required Packages:
+-       yfinance
+-       csv
+-       tkinter
+-       customtkinter
+-       matplotlib
+-       seaborn
+-       pandas
+-       datetime
+-
+-   Required Modules:
+-       getTicker_ctk.py
+-       numDays_ctk.py
+-       getCompanyData.py
+-       displayData_ctk.py
+-
+-   Methods:
+-       get_ticker()
+-       get_numdays()
+-       get_dates()
+-       get_data()
+-       plot_window()
+-       main()
+-
+-
+-   Jeff Canepa
+-   jeff.canepa@gmail.com
+-   Oct 2023
+--------------------------------------------------------------
+'''
 import plotStockModules.getTicker_ctk as getTicker
 import plotStockModules.numDays_ctk as numDays
 import plotStockModules.getCompanyData as getCompanyData
